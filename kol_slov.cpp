5490c50175633a30560525268cc75743c2ea1310
Mango <92181249+GitRaut@users.noreply.github.com>
2021-11-18 20:23:33 +0300

    Update kol_slov.cpp

--- kol_slov.cpp
+++ kol_slov.cpp
@@ -67,7 +67,14 @@ char itc_sameChar(string str){
 }
 
 bool itc_isFirstInSecond(string s1, string s2){
-	if (itc_find_str(s2, s1) != -1)
-		return true;
-	return false;
+    if(s1 == ""){
+        return false;
+    }
+    if(s2 == ""){
+        return false;
+    }
+	if (itc_find_str(s2, s1) == -1){
+		return false;
+	}
+	return true;
 }
