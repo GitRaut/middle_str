586a98270fce4b55e6d8b7f018f3e1c54f8b6aea
Mango <92181249+GitRaut@users.noreply.github.com>
2021-11-18 20:05:13 +0300

    Update kol_slov.cpp

--- kol_slov.cpp
+++ kol_slov.cpp
@@ -70,10 +70,8 @@ bool itc_isFirstInSecond(string s1, string s2){
     if(s1 == "" or s2 == ""){
         return false;
     }
-    for(int i = 0; itc_len(itc_slice_str(s2, i, i + itc_len(s1) - 1)) == itc_len(s1); i++){
-        if(itc_slice_str(s2, i, i + itc_len(s1) - 1) == s1){
-            return true;
-        }
-    }
-    return false;
+	if (itc_find_str(s2, s1) != -1)
+		return true;
+	return false;
+}
 }
